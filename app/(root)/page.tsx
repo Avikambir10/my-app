6adab249488619d77735ff0604ff01bcfbe458ce
Avikam <avikam198.be22@chitkara.edu.in>
2024-10-29 15:40:13 +0530

    Authentication and Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-    home
+   <section className="pink_container">
+    <h1 className="heading">Pitch Your Startup,<br/>Connect with entrepreneurs
+    </h1>
+
+    <p className="sub-heading !max-w-3xl">Submit ideas, Vote on Pitches, and get noticed in Virtual Competitions.</p>
+   </section>
+    
    </>
-   
   );
 }
